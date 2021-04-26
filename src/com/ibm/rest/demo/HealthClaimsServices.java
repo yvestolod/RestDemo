305bd71ecea1fdff08b20bb68b1038eb5a771228
Yves Tolod <yves.tolod@ca.ibm.com>
2021-04-26 09:45:35 -0400

    Update HealthClaimsServices.java

--- src/com/ibm/rest/demo/HealthClaimsServices.java
+++ src/com/ibm/rest/demo/HealthClaimsServices.java
@@ -1,4 +1,4 @@
-// (C) Copyright IBM Corp. 2020 All Rights Reserved
+// (C) Copyright IBM Corp. 2021 All Rights Reserved
 //
 // Licensed under the Apache License, Version 2.0
 // which you can read at https://www.apache.org/licenses/LICENSE-2.0
@@ -14,9 +14,9 @@
 // The insurance claims service will reject requests if the amount
 // exceeded the limit:
 //
-//   Drug claim - amount exceeded claim limit of $1000
-//   Dental claim - amount exceeded claim limit of $800
-//   Medical claim - amount exceeded claim limit of $500 
+//   Drug claim - amount exceeded claim limit of $800
+//   Dental claim - amount exceeded claim limit of $500
+//   Medical claim - amount exceeded claim limit of $300 
 //
 
 package com.ibm.rest.demo;
@@ -29,6 +29,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 
 @Path("/rule")
 public class HealthClaimsServices extends Application {
@@ -36,6 +37,10 @@ public class HealthClaimsServices extends Application {
 	private static final String MEDICAL = "MEDICAL";
 	private static final String DENTAL = "DENTAL";
 	private static final String DRUG = "DRUG";
+	private static final String MEDICAL_LIMIT = "300.00";
+	private static final String DENTAL_LIMIT = "500.00";
+	private static final String DRUG_LIMIT = "800.00";
+	private static final String VERSION = "5.0";
 	private static final int EXCEEDED_LIMIT = -1;
 	
 	@GET
@@ -45,30 +50,31 @@ public class HealthClaimsServices extends Application {
 		String jsonResponse = "";
 		String reason = "Normal claim";
 		String status = "Accepted";
-
-	    //  Drug claim - amount exceeded claim limit of $1000
-	    //  Dental claim - amount exceeded claim limit of $800
-	    //  Medical claim - amount exceeded claim limit of $500 		
-		BigDecimal drugLimit = new BigDecimal(1000.00);
-		BigDecimal medicalLimit = new BigDecimal(500.00);
-		BigDecimal dentalLimit = new BigDecimal(800.00);
+		Timestamp ts = new Timestamp(System.currentTimeMillis());
+		
+	    //  Drug claim - amount exceeded claim limit of $800
+	    //  Dental claim - amount exceeded claim limit of $500
+	    //  Medical claim - amount exceeded claim limit of $300 		
+		BigDecimal drugLimit = new BigDecimal(DRUG_LIMIT);
+		BigDecimal dentalLimit = new BigDecimal(DENTAL_LIMIT);
+		BigDecimal medicalLimit = new BigDecimal(MEDICAL_LIMIT);
 		
 		if (claimType != null && claimAmount != null) {
 			
 			if (MEDICAL.equalsIgnoreCase(claimType) && 
 				medicalLimit.compareTo(claimAmount) == EXCEEDED_LIMIT) {
 				status = "Rejected";
-				reason = "Amount exceeded $500. Claim require further review.";
+				reason = "Amount exceeded " + MEDICAL_LIMIT + ". Claim require further review.";
 			}
 			else if (DENTAL.equalsIgnoreCase(claimType) &&
 					 dentalLimit.compareTo(claimAmount) == EXCEEDED_LIMIT) {
 				status = "Rejected";
-				reason = "Amount exceeded $800. Claim require further review.";
+				reason = "Amount exceeded " + DENTAL_LIMIT + ". Claim require further review.";
 			} 
 			else if (DRUG.equalsIgnoreCase(claimType) &&
 					 drugLimit.compareTo(claimAmount) == EXCEEDED_LIMIT) {
 				status = "Rejected";
-				reason = "Amount exceeded $1000. Claim require further review.";
+				reason = "Amount exceeded " + DRUG_LIMIT + ". Claim require further review.";
 			} 
 			else {
 				status = "Accepted";
@@ -80,6 +86,11 @@ public class HealthClaimsServices extends Application {
                            ", \"status\" : \"" + status + 
                            "\", \"reason\" : \"" + reason + 
 					       "\"}" ;
+
+			System.out.println("[CLAIMAPP] Version " + VERSION + " " + ts + ": Claim = " + claimType.toUpperCase() + 
+					           ", Claim Amount = " + claimAmount +
+					           ", Status = " + status +
+					           ", Reason = " + reason);
 		}
 		else {
 			status = "Rejected";
